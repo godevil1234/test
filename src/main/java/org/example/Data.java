e9f00739ead499fbf0e9305d67ef3aa0a86c1324
Nikita <pozitif5000@mail.ru>
2024-06-20 23:08:56 +0500

    UpdateData

--- src/main/java/org/example/Data.java
+++ src/main/java/org/example/Data.java
@@ -16,34 +16,34 @@ public class Data {
         return String.format("%02d.%02d.%02d", day, month, year);
     }
     public static void main(String[] args) {
-        Data data = new Data(20, 6,2024);
-        data.addDays(47);
+        Data data = new Data(20,6,2024);
+        data.addDays(1000);
         System.out.println(data);
     }
     public void addDays (int additionalDays) {
         day += additionalDays;
-        while (day > daysInMonth()) {
-            day -= daysInMonth();
+        while (day > whichMonth()) {
+            day -= whichMonth();
             addMonths(1);
         }
     }
-    private void addMonths (int additionalMonths) {
+    public void addMonths (int additionalMonths) {
         month += additionalMonths;
         while (month > 12) {
             month -= 12;
             addYears(1);
         }
     }
-    private void addYears(int additionalYears) {
+    public void addYears(int additionalYears) {
         year += additionalYears;
     }
-    private int daysInMonth() {
-        if (month == 2 && isLeapYear(year)) {
+    public int whichMonth() {
+        if (month == 2 && leapYear(year)) {
             return 29;
         }
         return ALL_MONTHS[month];
     }
-    public boolean isLeapYear(int year) {
-        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+    public boolean leapYear(int year) {
+        return year % 400 == 0 && year % 100 == 0 || (year % 4 == 0 && year % 100 != 0);
     }
 }
